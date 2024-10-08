03490ff0627e8fdb161da1156f07ae3a0da40743
helios-code <loicmancino.work@gmail.com>
2024-10-08 17:58:24 +0200

    Add initial setup files and Dockerfile for DSOL
    Introduce `setup.py` for `commons` and `libs` to manage dependencies. Create `Dockerfile.dsol` to handle DSOL-specific configurations and dependencies.

---
+++ src/commons/setup.py
@@ -0,0 +1,9 @@
+# src/commons/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='commons',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+)
