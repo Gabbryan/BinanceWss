e0f149f3d8c5f26610a146ee6c8c58d4d2440073
helios-code <loicmancino.work@gmail.com>
2024-10-11 12:26:00 +0200

    Remove deprecated Dockerfiles and Spark configurations
    This commit deletes several unused Dockerfiles and related Spark configuration files from various directories. The removed files include Docker setup scripts for ingestion, transformation, validation, and stockage services, as well as Spark and Hadoop configuration files. This cleanup aims to eliminate unused resources and streamline the repository.

--- src/cores/cicada-ingestion/docker/base/spark-base/conf/spark-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export SPARK_MASTER_HOST=spark-master
-export SPARK_WORKER_MEMORY=20g    # Memory allocated for each worker
-export SPARK_WORKER_CORES=4       # Cores allocated for each worker
-export SPARK_WORKER_INSTANCES=2   # Number of worker instances per node
