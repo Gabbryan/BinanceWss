fc1f5b233d9d04f100355c9cb6d9d82e80367c2d
helios-code <loicmancino.work@gmail.com>
2024-10-22 12:59:45 +0200

    Add notifications for process start and end
    Introduced a notifications controller to send messages when the process starts and ends. This improves monitoring and tracking of the ingestion process.

--- src/cores/cicada-ingestion/data-providers/Binance/sources/ingestion-live-wss/main.py
+++ src/cores/cicada-ingestion/data-providers/Binance/sources/ingestion-live-wss/main.py
@@ -1,11 +1,13 @@
 from controller_binance_wss import BinanceWSSClient
 from src.commons.env_manager.env_controller import EnvController
+from src.commons.notifications.notifications_controller import NotificationsController
 from src.libs.utils.sys.threading.controller_threading import ThreadController
 
 # Initialize the controller for the 'development' environment
 env_controller = EnvController()
-
+notifications_controller = NotificationsController('wss-binance')
 if __name__ == "__main__":
+    notifications_controller.send_process_start_message()
     # Initialize the controller
     thread_controller = ThreadController()
     symbols = env_controller.get_yaml_config('wss-binance', 'symbols')
@@ -14,7 +16,6 @@ if __name__ == "__main__":
     for symbol in symbols:
         for stream in streams:
             binance_client = BinanceWSSClient(symbol, stream=stream)
-
             # Adding the client and specifying 'connect' method to run in thread
             thread_controller.add_thread(binance_client, method_name="connect")
 
@@ -23,3 +24,4 @@ if __name__ == "__main__":
 
     # Optionally, join all threads to stop them cleanly
     thread_controller.stop_all()
+    notifications_controller.send_process_end_message()
